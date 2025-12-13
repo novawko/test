96d4ec92e9ed8a600687341734ca6bba67379c5b
Nova Wolfcreek <novawko@gmail.com>
2025-12-12 23:44:58 -0600

    Update config.js

--- styles/js/config.js
+++ styles/js/config.js
@@ -429,7 +429,7 @@ charadex.page.masterlist = {
       sheetPage: charadex.sheet.pages.ownershipLog,
       primaryProperty: 'id',
       relatedProperty: 'id',
-      dexSelector: 'log',
+      dexSelector: 'ownershiplog',
       profileProperty: 'design',
       profileToggle: false,
 
