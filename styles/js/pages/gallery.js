50f1cc02e6d69698f1faf54ea510fe3f7029a2a5
Nova Wolfcreek <novawko@gmail.com>
2026-03-02 17:53:06 -0600

    Update gallery.js

--- styles/js/pages/gallery.js
+++ styles/js/pages/gallery.js
@@ -8,27 +8,22 @@ import { charadex } from '../charadex.js';
 /* Load
 ======================================================================= */
 document.addEventListener("DOMContentLoaded", async () => {
-
   let dex = await charadex.initialize.page(
     null, 
     charadex.page.imageGallery, 
-    (arr) => {
-      for (let entry of arr) {
+    null,
+    (listData) => {
+      if (listData.type == 'profile') {
+        
+        const profile = listData.profileArray[0];
 
-        // We're gonna make some badges but you dont have to use them
-        entry.designs = entry.designs.split(',');
-        entry.designBadges = [];
-        for (let design of entry.designs) {
-          entry.designBadges.push(
-            `<a class="badge badge-primary" href="${charadex.url.addUrlParameters(charadex.url.getPageUrl('masterlist'), {profile: design})}">${design.trim()}</a>`
-          );
-        }
-        entry.designBadges = entry.designBadges.join(' ');
+        linkSelectors.forEach(({ key, selector }) => {
+          if (profile[key]) $(selector).show();
+        });
+
+        if (profile.tags.length > 0) $("#charadex-profile-tags").show();
 
       }
-    }
-  );
-
+    });
   charadex.tools.loadPage('.softload', 500);
-
 });
