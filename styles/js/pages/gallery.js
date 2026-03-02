fe37c19dfc0dea5bd6e94df2baa4807fb3cb6390
Nova Wolfcreek <novawko@gmail.com>
2026-03-02 17:59:17 -0600

    Update gallery.js

--- styles/js/pages/gallery.js
+++ styles/js/pages/gallery.js
@@ -8,20 +8,14 @@ import { charadex } from '../charadex.js';
 /* Load
 ======================================================================= */
 document.addEventListener("DOMContentLoaded", async () => {
+
   let dex = await charadex.initialize.page(
     null, 
     charadex.page.imageGallery, 
-    null,
-    (listData) => {
-      if (listData.type == 'profile') {
-        
-        const profile = listData.profileArray[0];
+    (arr) => {
+      for (let entry of arr) {
 
-        if (profile.tags.length > 0) $("#charadex-profile-tags").show();
-
-      }
-
-      // We're gonna make some badges but you dont have to use them
+        // We're gonna make some badges but you dont have to use them
         entry.designs = entry.designs.split(',');
         entry.designBadges = [];
         for (let design of entry.designs) {
@@ -30,7 +24,13 @@ document.addEventListener("DOMContentLoaded", async () => {
           );
         }
         entry.designBadges = entry.designBadges.join(' ');
-      
-    });
+
+        if (profile.tags.length > 0) $("#charadex-profile-tags").show();
+
+      }
+    }
+  );
+
   charadex.tools.loadPage('.softload', 500);
+
 });
