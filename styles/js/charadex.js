28fcd30695df631875942e462c25d9896d762228
Nova Wolfcreek <novawko@gmail.com>
2026-03-02 17:03:55 -0600

    Update charadex.js

--- styles/js/charadex.js
+++ styles/js/charadex.js
@@ -38,7 +38,7 @@ charadex.initialize.page = async (dataArr, config, dataCallback, listCallback, c
     charadex.tools.addProfileLinks(entry, pageUrl, config.profileProperty); // Go ahead and add profile keys just in case
     if (folders) folders(entry, config.fauxFolder.folderProperty); // If folders, add folder info
     if (entry.rarity) entry.raritybadge = `<span class="badge badge-${charadex.tools.scrub(entry.rarity)}">${entry.rarity}</span>`; // Adds a rarity badge
-    if (tags) tags(entry, pageUrl, config.tags); // Make the tags if any
+    if (tags) tags(entry, pageUrl, config.tags.parameters); // Make the tags if any
   }
 
   // If there's related data, add it
