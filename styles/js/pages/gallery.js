e8854414d59ff114455af3b7707ca67ec9551531
Nova Wolfcreek <novawko@gmail.com>
2026-03-02 15:02:26 -0600

    Update gallery.js

--- styles/js/pages/gallery.js
+++ styles/js/pages/gallery.js
@@ -35,6 +35,8 @@ document.addEventListener("DOMContentLoaded", async () => {
         }
         entry.fancytags = fancyTagArr.join(' ');
 
+        if (profile.tags.length > 0) $("#charadex-profile-tags").show(); // Show them tags if any
+
       }
     }
   );
